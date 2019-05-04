892fb1bd6c325118812657efed1e999ce0c060e7
birden <dbir@ngs.ru>
2019-05-04 16:26:38 +0700

    First

---
+++ Learn/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn
+{
+    class Some
+    {
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine("Result:" + a * b);
+        }
+        public void Multiply(int a, int b, int c)
+        {
+            Console.WriteLine("Result:" + a * b * c);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Some test = new Some();
+            test.Multiply(2, 4);
+            test.Multiply(2, 4, 5);
+            Console.ReadKey();
+        }
+    }
+}
